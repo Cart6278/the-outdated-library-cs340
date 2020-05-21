d300dcfcc4ac3dcea2f011329769000fe6779c7f
Kirsten C <cartekir@oregonstate.edu>
2020-05-21 16:42:10 -0700

    Got Authors and Books add to show up. Database does not update with entered information yet.

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -29,23 +29,23 @@ def add_members():
     db_connection = connect_to_database()
 
     if request.method == 'GET':
-        query = 'SELECT id, name from bsg_planets'
-        result = execute_query(db_connection, query).fetchall()
-        print(result)
-
         return render_template('members_add.html')
-    elif request.method == 'POST':
-        print("Add new people!")
 
-        fname = request.form['fname']
-        lname = request.form['lname']
-        age = request.form['age']
-        homeworld = request.form['homeworld']
-        query = 'INSERT INTO bsg_people (fname, lname, age, homeworld) VALUES (%s,%s,%s,%s)'
-        data = (fname, lname, age, homeworld)
+    elif request.method == 'POST':
+        memberFirst = request.form['memberFirst']
+        memberLast = request.form['memberLast']
+        streetAddr = request.form['streetAddr']
+        city = request.form['city']
+        state = request.form['state']
+        postalCode = request.form['postalCode']
+        phoneNum = request.form['phoneNum']
+        email = request.form['email']
+
+        query = 'INSERT INTO Members (memberFirst, memberLast, streetAddr, city, state, postalCode, phoneNum, email) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+        data = (memberFirst, memberLast, streetAddr, city, state, postalCode, phoneNum, email)
         execute_query(db_connection, query, data)
 
-        return ('Member added!')
+        return render_template('members_add.html')
 
 @webapp.route('/reservations_browse.html')
 def browse_reservations():
@@ -56,6 +56,23 @@ def browse_reservations():
     result = execute_query(db_connection, query).fetchall()
     print(result)
     return render_template('reservations_browse.html')
+
+
+
+@webapp.route('/authors_add.html', methods=['POST','GET'])
+def add_authors():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        return render_template('authors_add.html')
+
+    elif request.method == 'POST':
+        authorFirst = request.form['authorFirst']
+        authorLast = request.form['authorLast']
+
+        query = 'INSERT INTO Authors (authorFirst, authorLast) VALUES (%s,%s)'
+        data = (authorFirst, authorLast)
+        execute_query(db_connection, query, data)
+        return render_template('authors_add.html')
 
 @webapp.route('/authors.html')
 #the name of this function is just a cosmetic thing
@@ -68,24 +85,24 @@ def browse_authors():
     print(result)
     return render_template('authors.html', rows=result)
 
-@webapp.route('/authors_add.html', methods=['POST','GET'])
-def add_authors():
+@webapp.route('/books_add.html', methods=['POST','GET'])
+def add_books():
     db_connection = connect_to_database()
     if request.method == 'GET':
-        query = 'SELECT id, name from Authors'
-        result = execute_query(db_connection, query).fetchall()
-        print(result)
+        return render_template('books_add.html')
 
-        return render_template('authors_add.html', author = result)
     elif request.method == 'POST':
-        print("Add new people!")
-        authorFirst = request.form['authorFirst']
-        authorLast = request.form['authorLast']
+        bookTitle = request.form['bookTitle']
+        bookAuthor = request.form['bookAuthor']
+        bookGenre= request.form['bookGenre']
+        bookFiction= request.form['bookFiction']
+        bookIsbn= request.form['bookIsbn']
 
-        query = 'INSERT INTO Authors (authorFirst, authorLast) VALUES (%s,%s)'
-        data = (authorFirst, authorLast)
+        query = 'INSERT INTO Books (bookTitle, bookAuthor, bookGenre, bookFiction, bookIsbn) VALUES (%s,%s,%s,%s,%s)'
+        data = (bookTitle, bookAuthor, bookGenre, bookFiction, bookIsbn)
         execute_query(db_connection, query, data)
-        return ('Author added!')
+
+        return render_template('books_add.html')
 
 @webapp.route('/books.html')
 #the name of this function is just a cosmetic thing
@@ -97,28 +114,6 @@ def browse_books():
     result = execute_query(db_connection, query).fetchall()
     print(result)
     return render_template('books.html')
-
-@webapp.route('/books_add.html', methods=['POST','GET'])
-def add_books():
-    db_connection = connect_to_database()
-    if request.method == 'GET':
-        query = 'SELECT id, name from Authors'
-        result = execute_query(db_connection, query).fetchall()
-        print(result)
-
-        return render_template('books_add.html', author = result)
-    elif request.method == 'POST':
-        print("Add new Books!")
-#	bookTitle = request.form['bookTitle'] bookAuthor = request.form['bookAuthor']
-#	bookAuthor = request.form['bookAuthor']
-#	bookGenre= request.form['bookGenre']
-#	bookFiction= request.form['bookFiction']
-# 	bookIsbn= request.form['bookIsbn']
-
-#	query = 'INSERT INTO Books (bookTitle, bookAuthor, bookGenre, bookFiction, bookIsbn) VALUES (%s,%s,%s,%s,%s)'
-#	data = (bookTitle, bookAuthor, bookGenre, bookFiction, bookIsbn)
-#       execute_query(db_connection, query, data)
-        return ('Book added!')
 
 # Library landing page
 @webapp.route('/')
