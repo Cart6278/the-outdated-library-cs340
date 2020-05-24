5d5ea5d9ac4f94a6b6ec56f82524319df52a3d23
michaeldton <46663814+michaeldton@users.noreply.github.com>
2020-05-23 19:06:32 -0500

    books display fixed, reservations INSERT/SELECT/DELETE implemented
    delete needs to be fixed/improved on Reservations

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -5,26 +5,18 @@ from db_connector.db_connector import connect_to_database, execute_query
 webapp = Flask(__name__)
 webapp.config['DEBUG'] = True
 
-# EXAMPLE: 
-# provide a route where requests on the web application can be addressed
-# @webapp.route('/hello')
-# provide a view (fancy name for a function) which responds to any requests on this route
-# def hello():
-#    return "Hello World!"
-#
-
-@webapp.route('/members_browse.html')
+@webapp.route('/members_browse')
 #the name of this function is just a cosmetic thing
 def browse_members():
-    print("Fetching and rendering members web page")
-    db_connection = connect_to_database()
-    query = 'SELECT * FROM Members;'
+        db_connection = connect_to_database()
+        print("Fetching and rendering members web page")
+        query = 'SELECT * FROM Members;'
 
-    result = execute_query(db_connection, query).fetchall()
-    print(result)
-    return render_template('members_browse.html', rows=result)
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
+        return render_template('members_browse.html', rows=result)
 
-@webapp.route('/members_add.html', methods=['POST','GET'])
+@webapp.route('/members_add', methods=['POST','GET'])
 def add_members():
     db_connection = connect_to_database()
 
@@ -47,22 +39,63 @@ def add_members():
 
         return render_template('members_add.html')
 
-@webapp.route('/reservations_browse.html')
+@webapp.route('/reservations_browse')
 def browse_reservations():
     print("Fetching and rendering reservations web page")
     db_connection = connect_to_database()
-    query = 'SELECT r.reservationID, r.memberID, m.memberFirst, m.memberLast, b.title, r.bookID, r.dateIssued, r.dateDue, (SELECT IF(isReturned, \'Yes\', \'No\')) FROM Reservations AS r LEFT JOIN Members AS m ON r.memberID=m.memberID LEFT JOIN Book_Items AS bi ON r.bookID=bi.bookID LEFT JOIN Books AS b ON bi.isbn=b.isbn;'
+    query = ''' SELECT r.reservationID, r.memberID, m.memberFirst, m.memberLast, b.title, r.bookID, r.dateIssued, r.dateDue, (SELECT IF(isReturned, \'Yes\', \'No\')) FROM Reservations AS r 
+		LEFT JOIN Members AS m ON r.memberID=m.memberID 
+		LEFT JOIN Book_Items AS bi ON r.bookID=bi.bookID 
+		LEFT JOIN Books AS b ON bi.isbn=b.isbn; '''
 
     result = execute_query(db_connection, query).fetchall()
     print(result)
     return render_template('reservations_browse.html', rows=result)
+
+@webapp.route('/reservations_add', methods=['POST', 'GET'])
+def add_reservations():
+    db_connection = connect_to_database()
+
+    if request.method == 'GET':
+        return render_template('reservations_add.html')
+
+    elif request.method == 'POST':
+        memberFirst = request.form['memberFirst']
+        memberLast = request.form['memberLast']
+        isbn = request.form['isbn']
+        dateIssued = request.form['dateIssued']
+        dateDue = request.form['dateDue']
+        isReturned = request.form['isReturned']
+
+        query = ''' INSERT INTO Reservations (memberID, bookID, dateIssued, dateDue, isReturned) VALUES 
+			((SELECT memberID FROM Members WHERE memberFirst=%s AND memberLast=%s),
+		 	 (SELECT bi.bookID FROM Book_Items AS bi LEFT JOIN Books AS b ON bi.isbn=b.isbn WHERE b.isbn = %s),
+		 	 %s,%s,%s) '''
+        data = (memberFirst, memberLast, isbn, dateIssued, dateDue, isReturned)
+        execute_query(db_connection, query, data)
+
+        return render_template('reservations_add.html')
+
+#
+# Need to fix page re-direction after delete
+#
+
+@webapp.route('/reservations_browse/<int:id>')
+#the name of this function is just a cosmetic thing
+def delete_reservations(id):
+        db_connection = connect_to_database()
+        query = "DELETE FROM Reservations WHERE reservationID = %s"
+        data = (id,)
+
+        result = execute_query(db_connection, query, data)
+        return browse_reservations()
 
 @webapp.route('/authors.html')
 #the name of this function is just a cosmetic thing
 def browse_authors():
     print("Fetching and rendering authors web page")
     db_connection = connect_to_database()
-    query = 'SELECT * FROM Authors;'
+    query = 'SELECT a.authorID, a.authorFirst, a.authorLast FROM Authors AS a;'
 
     result = execute_query(db_connection, query).fetchall()
     print(result)
@@ -92,8 +125,10 @@ def add_authors():
 def browse_books():
     print("Fetching and rendering books web page")
     db_connection = connect_to_database()
-    query = 'SELECT * FROM Books;'
-
+    query = ''' SELECT b.isbn, b.title, GROUP_CONCAT(DISTINCT a.authorFirst, ' ', a.authorLast) AS authorName, b.genre, b.isFiction FROM Authors AS a 
+		INNER JOIN Author_Book AS ab ON ab.authorID=a.authorID 
+		INNER JOIN Books AS b ON ab.isbn=b.isbn 
+		GROUP BY b.title ORDER BY a.authorFirst, a.authorLast; '''
     result = execute_query(db_connection, query).fetchall()
     print(result)
     return render_template('books.html', rows=result)
